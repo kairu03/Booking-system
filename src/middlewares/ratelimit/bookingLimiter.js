0f4663ab6fb1b60166aa813de70b611986e14722
kairufrancisco <kairufrancisco@gmail.com>
2026-03-22 19:22:05 +0800

    add rate limiting for booking actions and global requests

---
+++ src/middlewares/ratelimit/bookingLimiter.js
@@ -0,0 +1,24 @@
+import { createLimiter } from "./createLimiter.js";
+
+export const createBookingLimiter = createLimiter(
+  `booking:create`,
+  5,
+  60 * 15,
+  'Too many booking attempts, try again later',
+  { useUserId: true },
+);
+
+export const updateBookingLimiter = createLimiter(
+  'booking:update',
+  5,
+  60 * 15,
+  'Too many booking updates, try again later',
+  { useUserId: true },
+);
+
+export const cancelBookingLimiter = createLimiter(
+  `booking:cancel`,
+  5,
+  60 * 15,
+  'Too many cancellations, try again later',
+);
